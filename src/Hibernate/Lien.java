e0ac98666d93de9765dc5a47297079d12b634796
luc.chereau <luc.chereau@etu.univ-tours.fr>
2022-01-18 15:14:26 +0100

    Update lien motcle

--- src/Hibernate/Lien.java
+++ src/Hibernate/Lien.java
@@ -8,5 +8,29 @@ public class Lien {
 	private String adresse_Lien; 
 	private String texte; 
 	@ManyToOne(cascade = CascadeType.ALL)
-	private Message m; 
+	private Message m;
+	public int getId_lien() {
+		return id_lien;
+	}
+	public void setId_lien(int id_lien) {
+		this.id_lien = id_lien;
+	}
+	public String getAdresse_Lien() {
+		return adresse_Lien;
+	}
+	public void setAdresse_Lien(String adresse_Lien) {
+		this.adresse_Lien = adresse_Lien;
+	}
+	public String getTexte() {
+		return texte;
+	}
+	public void setTexte(String texte) {
+		this.texte = texte;
+	}
+	public Message getM() {
+		return m;
+	}
+	public void setM(Message m) {
+		this.m = m;
+	} 
 }
--- src/Hibernate/Mot_Cle.java
+++ src/Hibernate/Mot_Cle.java
@@ -4,5 +4,17 @@ import javax.persistence.*;
 public class Mot_Cle {
 	private String Contenu_mot_cle; 
 	@ManyToOne(cascade = CascadeType.ALL)
-	private Message m; 
+	private Message m;
+	public String getContenu_mot_cle() {
+		return Contenu_mot_cle;
+	}
+	public void setContenu_mot_cle(String contenu_mot_cle) {
+		Contenu_mot_cle = contenu_mot_cle;
+	}
+	public Message getM() {
+		return m;
+	}
+	public void setM(Message m) {
+		this.m = m;
+	} 
 }
