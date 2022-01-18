ad999a7538faf373c8b2eadae9fa7a9ccf9c36a3
luc.chereau <luc.chereau@etu.univ-tours.fr>
2022-01-18 15:13:12 +0100

    Update Mot_Cle

--- src/Hibernate/Mot_Cle.java
+++ src/Hibernate/Mot_Cle.java
@@ -1,5 +1,8 @@
 package Hibernate;
-
+import javax.persistence.*; 
+@Entity
 public class Mot_Cle {
-
+	private String Contenu_mot_cle; 
+	@ManyToOne(cascade = CascadeType.ALL)
+	private Message m; 
 }
