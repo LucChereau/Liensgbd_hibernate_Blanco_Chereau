3c4173de6295301746c092548323f3f8176fd3b9
luc.chereau <luc.chereau@etu.univ-tours.fr>
2022-01-18 14:59:19 +0100

    Update Adresse.java

--- src/Hibernate/Adresse.java
+++ src/Hibernate/Adresse.java
@@ -1,5 +1,67 @@
 package Hibernate;
 
-public class Adresse {
+import javax.persistence.*;
 
+@Entity
+public class Adresse {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	
+	private String City;
+	private String Street;
+	private String State;
+	private String PostalCode;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	private Utilisateur p;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCity() {
+		return City;
+	}
+
+	public void setCity(String city) {
+		City = city;
+	}
+
+	public String getStreet() {
+		return Street;
+	}
+
+	public void setStreet(String street) {
+		Street = street;
+	}
+
+	public String getState() {
+		return State;
+	}
+
+	public void setState(String state) {
+		State = state;
+	}
+
+	public String getPostalCode() {
+		return PostalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		PostalCode = postalCode;
+	}
+
+	public Utilisateur getP() {
+		return p;
+	}
+
+	public void setP(Utilisateur p) {
+		this.p = p;
+	}
 }
