fff321f24a5411deb8d28331554e6831ac070203
luc.chereau <luc.chereau@etu.univ-tours.fr>
2022-01-18 15:11:10 +0100

    Update Lien

--- src/Hibernate/Lien.java
+++ src/Hibernate/Lien.java
@@ -1,5 +1,12 @@
 package Hibernate;
-
+import javax.persistence.*; 
+@Entity
 public class Lien {
-
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id_lien;
+	private String adresse_Lien; 
+	private String texte; 
+	@ManyToOne(cascade = CascadeType.ALL)
+	private Message m; 
 }
