3c8f4d021ce7bda4ff60e53d7be68fca901d8674
luc.chereau <luc.chereau@etu.univ-tours.fr>
2022-01-19 17:08:47 +0100

    push

--- src/Hibernate/LienDAO.java
+++ src/Hibernate/LienDAO.java
@@ -4,9 +4,9 @@ import javax.persistence.EntityManager;
 
 public class LienDAO {
 	public static void Create_Lien(EntityManager em, String Adresse_lien, String texte, Message m) {
-		Lien l=new Message(titre,texte,Date_de_post,u); 
+		Lien l=new Lien(Adresse_lien,texte,m); 
 		em.getTransaction().begin();
-		em.persist(m);
+		em.persist(l);
 		em.getTransaction().commit();
 	}
 }
