ba8fb0ed64cc5be7ebad119a2f1908b37cb92275
luc.chereau <luc.chereau@etu.univ-tours.fr>
2022-01-19 15:33:31 +0100

    Update message

--- src/Hibernate/MessageDAO.java
+++ src/Hibernate/MessageDAO.java
@@ -11,8 +11,23 @@ public class MessageDAO {
 		em.getTransaction().commit();
 	}
 	
-	public static void Supprimer_Message(EntityManager em) {
-		
+	public static void Supprimer_Message(EntityManager em ,Message m) {
+		em.createQuery("delete from Message where id = :id")
+		  .setParameter("id", m.getId())
+		  .executeUpdate();
+	}
+	
+	public static Message GetMessage_from_title(EntityManager em, String titre) {
+		String hql = "from Message m where m.titre = :titre";
+		Query q = em.createQuery(hql);
+		q.setParameter("titre", titre);
+		List<Message> result = q.getResultList(); 
+		if(result.size()==0) {
+			return null; 
+		}
+		else {
+		return result.get(0);
+		}
 	}
 	
 	public static void Modifier_Message(EntityManager em) {
