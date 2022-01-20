6d9304f3ed6e21118acff062677bc35cc7bdd688
luc.chereau <luc.chereau@etu.univ-tours.fr>
2022-01-20 14:56:29 +0100

    supprimer Image

--- src/Hibernate/ImageDAO.java
+++ src/Hibernate/ImageDAO.java
@@ -1,6 +1,7 @@
 package Hibernate;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 import org.hibernate.type.BlobType;
 
@@ -10,5 +11,31 @@ public class ImageDAO {
 		em.getTransaction().begin();
 		em.persist(I);
 		em.getTransaction().commit();
+	}
+	
+	public static boolean Verify_Image_from_Message(Message m , Image I ) {
+		boolean t=false; 
+		for (int i=0; i<m.getListe_Image().size();i++) {
+			if(m.getListe_Image().get(i)==I) {
+				t=true; 
+			}
+		}
+		return t; 
+	}
+	
+	public static void Modify_Lien_serveur_Image(EntityManager em, Message m, Image I) {
+		
+	}
+	
+	public static void Supprimer_Image(EntityManager em, Message m, Image I) {
+		if(Verify_Image_from_Message(m,I)==true) {
+			em.getTransaction().begin();
+			em.persist(I);
+			String hql="delete from Image where id_Image = :id"; 
+			Query q=em.createQuery(hql); 
+			q.setParameter("id", I.getId_Image()); 
+			q.executeUpdate();
+			em.getTransaction().commit();
+		}
 	}
 }
